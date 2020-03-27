cf885b3f8a83af6cb999a3b753193c8ac89ab40c
Olivia Thetgyi <othetgyi@gmail.com>
2020-03-27 16:25:08 +0000

    Fixed errors and now this test and code for user story 1 is working

--- jasmine-standalone-3.5.0/spec/roomSpec.js
+++ jasmine-standalone-3.5.0/spec/roomSpec.js
@@ -8,86 +8,35 @@ describe('Room', function(){
     room = new Room();
   });
 
-  it('starts out by being available', function(){
-    expect(room.IsRoomAvailable).toEqual(true);
-  });
-
-  it('becomes unavailable when a person enters the room', function(){
-    room.enter();
-    expect(room.IsRoomAvailable).toEqual(false);
-  });
-
-  it('becomes available when a person leaves the room', function(){
-    room.enter();
-    room.leave();
-    expect(room.isRoomAvailable).toEqual(true);
-  });
-
-  describe('#enter', function(){
-    it('blocks someone entering the room when it is already occupied', function(){
-      expect(function(){
-        room.enter();
-      }).toThrowError('This room is already occupied');
-    });
+  it('is available', function(){
+    expect(room.isAvailable()).toBe(true);
   });
 });
 
 
-// describe("Player", function() {
-//   var player;
-//   var song;
-//
-//   beforeEach(function() {
-//     player = new Player();
-//     song = new Song();
-//   });
-//
-//   it("should be able to play a Song", function() {
-//     player.play(song);
-//     expect(player.currentlyPlayingSong).toEqual(song);
-//
-//     //demonstrates use of custom matcher
-//     expect(player).toBePlaying(song);
-//   });
-//
-//   describe("when song has been paused", function() {
-//     beforeEach(function() {
-//       player.play(song);
-//       player.pause();
-//     });
-//
-//     it("should indicate that the song is currently paused", function() {
-//       expect(player.isPlaying).toBeFalsy();
-//
-//       // demonstrates use of 'not' with a custom matcher
-//       expect(player).not.toBePlaying(song);
-//     });
-//
-//     it("should be possible to resume", function() {
-//       player.resume();
-//       expect(player.isPlaying).toBeTruthy();
-//       expect(player.currentlyPlayingSong).toEqual(song);
-//     });
-//   });
-//
-//   // demonstrates use of spies to intercept and test method calls
-//   it("tells the current song if the user has made it a favorite", function() {
-//     spyOn(song, 'persistFavoriteStatus');
-//
-//     player.play(song);
-//     player.makeFavorite();
-//
-//     expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
-//   });
-//
-//   //demonstrates use of expected exceptions
-//   describe("#resume", function() {
-//     it("should throw an exception if song is already playing", function() {
-//       player.play(song);
-//
-//       expect(function() {
-//         player.resume();
-//       }).toThrowError("song is already playing");
-//     });
-//   });
+
+
+
+
+
+  // it('can accept a name for the room', function(){
+  //   expect(room.getRoomName('Blue')).toEqual('Blue');
+  // });
+
+  // it('becomes unavailable when a person enters the room', function(){
+  //   room.enter();
+  //   expect(room.isRoomAvailable).toEqual(false);
+  // });
+  //
+  // it('becomes available when a person leaves the room', function(){
+  //   room.enter();
+  //   room.leave();
+  //   expect(room.isRoomAvailable).toEqual(true);
+  // });
+
+  // it('blocks someone from entering the room when it is already occupied', function(){
+  //   expect(function(){
+  //   room.enter();
+  //   }).toThrowError('This room is already occupied');
+  // });
 // });
--- jasmine-standalone-3.5.0/src/room.js
+++ jasmine-standalone-3.5.0/src/room.js
@@ -3,46 +3,21 @@
 class Room {
   constructor() {
     this.available = true;
-  }
-
-  isRoomAvailable() {
+  };
+  isAvailable(){
     return this.available;
-  }
-
-  enter(){
-    if (this.isRoomAavailable == true) {
-      this.isRoomAvailable = false;
-    } else {
-      throw new Error ('This room is already occupied');
-    }
-  }
-
-  leave(){
-    this.isRoomAvailable = true;
-  }
-}
+  };
+};
 
 
-
-// function Player() {
-// }
-// Player.prototype.play = function(song) {
-//   this.currentlyPlayingSong = song;
-//   this.isPlaying = true;
-// };
-//
-// Player.prototype.pause = function() {
-//   this.isPlaying = false;
-// };
-//
-// Player.prototype.resume = function() {
-//   if (this.isPlaying) {
-//     throw new Error("song is already playing");
-//   }
-//
-//   this.isPlaying = true;
-// };
-//
-// Player.prototype.makeFavorite = function() {
-//   this.currentlyPlayingSong.persistFavoriteStatus(true);
-// };
+  // enter(){
+  //   if (this.isRoomAavailable == true) {
+  //     this.isRoomAvailable = false;
+  //   } else {
+  //     throw new Error ('This room is already occupied');
+  //   }
+  // }
+  //
+  // leave(){
+  //   this.isRoomAvailable = true;
+  // }
